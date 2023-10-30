c6a16ad91be10c84aadb99119b7be8fc1fe6ca14
AyoubAaddam <144946172+AyoubAaddam@users.noreply.github.com>
2023-10-30 16:49:17 +0100

    Add files via upload

---
+++ idiomas/en.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+$lang = array(
+    'Titulo' => 'Ayoub School',
+    'ajustes' => 'Settings',
+    'perfil' => 'Profile',
+    'Tit1' => 'Welcome to Ayoub School',
+    'texto1' => 'We are a school committed to quality education and offer a Web Development (DAW) course to prepare you for the world of web programming.',
+    'Tit2' => 'School Information',
+    'texto2' => 'Our school is dedicated to providing high-quality comprehensive education. Here you can find more information about our history, mission, and vision.',
+    'Tit3' => 'Web Development (DAW) Course',
+    'texto3' => 'Our DAW course will equip you with the necessary skills to develop modern web applications. You will learn programming, web design, and more.',
+    'Contacto' => 'Contact',
+    'Contacto_texto' => 'You can reach us at:
+    Address: Middle Street Phone: 99999999999999 Email: ayoub@gmail.com'
+);
+
+
+
+?>
---
+++ idiomas/es.php
@@ -0,0 +1,19 @@
+<?php   
+
+$lang = array(
+
+    'Titulo'=> 'Colegio Ayoub',
+    'ajustes'=>'Ajustes',
+    'perfil'=> 'Perfil',
+    'Tit1'=>'Bienvenidos a Colegio Ayoub',
+    'texto1'=> 'Somos un colegio comprometido con la educación de calidad y ofrecemos un curso de Desarrollo de Aplicaciones Web (DAW) para prepararte en el mundo de la programación web.',
+    'Tit2'=>'Información del Colegio',
+    'texto2'=> 'Nuestro colegio se dedica a brindar una educación integral de alta calidad. Aquí puedes obtener más información sobre nuestra historia, misión y visión.',
+    'Tit3'=>'Curso de Desarrollo de Aplicaciones Web (DAW)',
+    'texto3'=>'Nuestro curso de DAW te proporcionará las habilidades necesarias para desarrollar aplicaciones web modernas. Aprenderás programación, diseño web y más.',
+    'Contacto'=>'Contacto',
+    'Contacto_texto'=> 'Puedes contactarnos en:
+    Dirección: Calle del medio Teléfono: 99999999999999 Correo electrónico: ayoub@gmail.com '
+);
+
+?>
