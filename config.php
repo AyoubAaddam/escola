a52788cf723f3a5d22405eae76e70c88ee650a3e
AyoubAaddam <144946172+AyoubAaddam@users.noreply.github.com>
2023-10-30 16:50:10 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Valores de acceso a la base de datos de MySQL
+$dbhost = "localhost";
+$dbname = 'escola';
+$dbuser = 'root';
+$dbpass = 'linuxlinux';
+$dsn='mysql:host='.$dbhost.';dbname='.$dbname.';';
+$driver = "mysql";
+
+?>
