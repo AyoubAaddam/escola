54012f2875198aa95a86eec445b4c41e5712328c
AyoubAaddam <144946172+AyoubAaddam@users.noreply.github.com>
2023-10-30 16:50:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+session_start();
+
+// Se incluye el enrutador y las rutas
+require 'src/router.php';
+require 'src/routes.php';
+require 'src/db.php';
+
+//$db=connect($dsn,$dbuser,$dbpass);
+//die;
+
+if(!isset($_SESSION['lang'])){
+    $_SESSION['lang']='es';
+    $_SESSION['home'] = 'Pagina Principal';
+}
+
+
+try {
+    // Obtenemos la ruta
+    $controller = getRoute($routes);
+
+    // En caso de que la ruta esté vacía, se asigna el controlador por defecto (home)
+    // En caso de que la ruta esté definida, se asigna el controlador correspondiente
+    // Si la ruta no existe, se lanza una excepción y se muestra la página de error
+    $controller == null ? $controller = 'home' : $controller;
+
+    // Se incluye el controlador correspondiente
+    require 'controllers/' . $controller . 'Controller.php';
+} catch (Exception $e) {
+    $_SESSION['error'] = $e->getMessage();
+    require 'public/error.php';
+}
+
+
+
+?>
